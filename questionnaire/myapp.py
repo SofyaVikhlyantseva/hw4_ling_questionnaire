029b2b60315bbcf67b0fe189765c7e98ab23a382
SofyaVikhlyantseva <113455552+SofyaVikhlyantseva@users.noreply.github.com>
2023-12-30 16:12:08 +0300

    change the path to the database in myapp.py

--- questionnaire/myapp.py
+++ questionnaire/myapp.py
@@ -12,8 +12,8 @@ matplotlib.use('agg')  # чтобы избежать UserWarning: Starting a Mat
 
 app = Flask(__name__)  # instance Flaskа
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///questionnaire.db'  # подключение БД
-# (sqlite:/// - это тип базы)
+    'sqlite:////home/sofyavikhlyantseva/hw4_ling_questionnaire/questionnaire/questionnaire.db'  # подключение БД
+# (sqlite:/// - это тип базы; относительные пути не работают, поэтому указываем абсолютный)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # чтобы нормально работать с изменениями в базе (коммиты и
 # обновления информации, если такие есть)
 
